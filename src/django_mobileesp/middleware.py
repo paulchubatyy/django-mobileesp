e7e1b5315f4c43357afcc7fe2b68f88021c4336e
Paul (xobb) Chubatyy <xobb@citylance.biz>
2023-12-20 22:13:11 -0800

    Fixing django 4+ compat

--- src/django_mobileesp/middleware.py
+++ src/django_mobileesp/middleware.py
@@ -8,11 +8,14 @@ def lazy_detection(request, key):
     return SimpleLazyObject( lambda: detector(request) )
 
 
-class UserAgentDetectionMiddleware(object):
+class UserAgentDetectionMiddleware:
+    def __init__(self, get_response):
+        self.get_response = get_response
     """
     Middleware to detect request's user agent
     """
-    def process_request(self, request):
+    def __call__(self, request):
         for each in DETECT_USER_AGENTS:
             setattr( request, each, lazy_detection(request, each) )
+        return self.get_response(request)
 
